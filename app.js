faf3b13c9150fe77d6ba110a25d46857e09e8f33
Pavel Silber <34262772+silberpavel@users.noreply.github.com>
2018-11-06 09:54:35 +0200

    nodemon has been added
    nodemon => Dev dependencies
    In package.json has been added script command (dev: nodemon that start app.js)

---
+++ app.js
@@ -0,0 +1,24 @@
+//Server
+const express = require('express')
+const path = require('path')
+
+const port = process.env.PORT || 5000
+const clientPath = path.join(__dirname, 'client')
+
+const app = express()
+app.use(express.static(clientPath))
+
+app.listen(port, () => {
+    console.log(`Server has been run on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
